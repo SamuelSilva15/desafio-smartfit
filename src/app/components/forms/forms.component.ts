eb87e98850be5e51f8b05e11572afe3293f5b5a3
samuel.silva <samuel.silva@azi.com.br>
2025-02-09 20:15:15 -0400

    feat: create form structure and style

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  imports: [],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent {
+  results: [] = [];
+
+  constructor() {}
+
+  ngOnInit():void {}
+}
